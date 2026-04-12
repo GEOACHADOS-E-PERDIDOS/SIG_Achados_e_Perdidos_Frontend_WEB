33fe64f08821261f4fc2b8a344eb46b1e968c9c3
KhalilFreitasIII <khalil.pimentel@estudante.ifb.edu.br>
2026-04-12 17:47:16 -0300

    Corrigindo Mapa

--- src/components/Mapa.tsx
+++ src/components/Mapa.tsx
@@ -1,27 +1,23 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, WMSTileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function Mapa() {
   return (
     <MapContainer
-      center={[-15.79, -47.88]} // Brasília (ajusta se quiser)
+      center={[-15.79, -47.88]}
       zoom={13}
       style={{ height: "100%", width: "100%" }}
     >
 
-      {/* 🌍 Mapa base */}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {/* 📍 Seus dados do GeoServer */}
-      <TileLayer
-        url="http://localhost:8080/geoserver/geoachados/wms"
-        params={{
-          layers: "geoachados:objetos",
-          format: "image/png",
-          transparent: true
-        }}
+      <WMSTileLayer
+        url="http://localhost:5538/geoserver/wms"
+        layers="Geoachados:objeto"
+        format="image/png"
+        transparent={true}
       />
 
     </MapContainer>
