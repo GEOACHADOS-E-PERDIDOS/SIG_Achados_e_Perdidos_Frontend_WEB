64c4362585fcd3d7f2591030bb6cb7181da0f48c
KhalilFreitasIII <khalil.pimentel@estudante.ifb.edu.br>
2026-05-03 21:11:18 -0300

    Design Mapa

--- src/components/Mapa.tsx
+++ src/components/Mapa.tsx
@@ -1,24 +1,42 @@
-import { MapContainer, TileLayer, WMSTileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, WMSTileLayer, LayersControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+const { Overlay, BaseLayer } = LayersControl;
 
 function Mapa() {
   return (
     <MapContainer
-      center={[-15.79, -47.88]}
-      zoom={13}
+      center={[-15.7939, -47.8828]}
+      zoom={14}
       style={{ height: "100%", width: "100%" }}
     >
+      <LayersControl position="bottomleft">
 
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        <BaseLayer checked name="Satélite">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution="Tiles © Esri"
+          />
+        </BaseLayer>
 
-      <WMSTileLayer
-        url="http://localhost:5538/geoserver/wms"
-        layers="Geoachados:objeto"
-        format="image/png"
-        transparent={true}
-      />
+        <BaseLayer name="Mapa">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="© OpenStreetMap"
+          />
+        </BaseLayer>
+
+        <Overlay checked name="Objetos Perdidos">
+          <WMSTileLayer
+            url="/geoserver/wms"
+            layers="Geoachados:objeto_perdido"
+            format="image/png"
+            transparent={true}
+            version="1.1.0"
+          />
+        </Overlay>
+      </LayersControl>
+
 
     </MapContainer>
   );
